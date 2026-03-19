f08a2c059f5ac592417e9f5aa3a1956548de4b1a
datahaki <jan.hakenberg@gmail.com>
2026-03-19 13:22:01 +0100

    minor

--- src/main/java/ch/alpine/owladd/klotzki/SunshineDemo.java
+++ src/main/java/ch/alpine/owladd/klotzki/SunshineDemo.java
@@ -17,7 +17,6 @@ import ch.alpine.tensor.red.Times;
     IO.println("NON BLOCKING");
     Tensor model2Pixel = klotskiDemo.getWindow().geometricComponent.getModel2Pixel();
     klotskiDemo.getWindow().geometricComponent.setModel2Pixel(Times.of(Tensors.vector(0.4, 0.4, 1), model2Pixel));
-    // klotskiDemo.timerFrame.geometricComponent.setOffset(100, 600);
     KlotskiSolution klotskiSolution = klotskiDemo.compute();
     Export.object(KlotskiDemo.solutionFile(klotskiProblem), klotskiSolution);
     klotskiDemo.close();
